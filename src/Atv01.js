395611b676bb2600bf886e567161ee5811473dce
Eduardo L Silva <els.fatec@gmail.com>
2023-10-19 23:44:57 -0300

    Add files via upload

--- src/Atv01.js
+++ src/Atv01.js
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import './Letreiro.css'
 
 function Atv01() {
-    return (
-      < > 
+  const frase = "Venha Estudar na FATEC  ";
+  const [letraIndex, setLetraIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLetraIndex((prevIndex) => (prevIndex + 1) % frase.length);
+    }, 300);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    < > 
           <h1> Atividade 01</h1>
           <Link to="/">Rertonar para a pagina Inicial</Link>
-      </>
-    ); 
-    }
-    export default Atv01;
+      
+    <div className='letreiro'>
+      <h1>{frase.substring(0, letraIndex)}</h1>
+    </div>
+    </>
+  );
+}
+
+export default Atv01;
+
+
